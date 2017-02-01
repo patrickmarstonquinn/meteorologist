bf9369b52d173c066b76af3522a4c0fcc9ffc978
patrickmarstonquinn <patrickmarstonquinn@users.noreply.github.com>
2017-01-31 23:47:36 -0600

    16%

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -8,17 +8,11 @@ class GeocodingController < ApplicationController
 
   def street_to_coords
     @street_address = params[:user_street_address]
-    @cleanstreet = @street_address.gsub(' ', '+')
-
-    # @url = "https://maps.googleapis.com/maps/api/geocode/json?address" + @cleanstreet +"&sensor=false"
-    @url = "https://maps.googleapis.com/maps/api/geocode/json?#{@cleanstreet}"
-
-    @parsed_data = JSON.parse(open(@url).read)
-    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
-
-    @latitude = "your text"
-    @longitude = "your text"
+    @street = @street_address.gsub(' ', '+')
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" << @street << "&sensor=false"
+    @parsed_data = JSON.parse(open(url).read)
+    @latitude = @parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = @parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
